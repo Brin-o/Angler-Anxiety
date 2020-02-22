cf16d0ad13620febf478304c273a8d875a4edb1b
Brin <brin@zvan.cc>
2020-02-22 02:55:17 +0100

    Player Controller
    Ustvarjeni naslednji:
    - player movement controller
    - player jaw controller (dodaj colliderje)
    - speed based scaling
    - rotation towards direction (rahlo buggy)

---
+++ Assets/Scripts/ScaleByVelocity.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using DG.Tweening;
+public class ScaleByVelocity : MonoBehaviour
+{
+	public enum Axis { X, Y }
+
+	public float bias = 1f;
+	public float strength = 1f;
+	public Axis axis = Axis.Y;
+
+	public new Rigidbody2D rigidbody;
+	[Range(0, 1)]
+	public float velocityGate = 0.2f;
+	[SerializeField] float tweenTimer = 0.5f;
+	[SerializeField] float slowTweenTimer = 0.1f;
+	[SerializeField] float yScaleLimit = 1.2f;
+	[SerializeField] float xScaleLimit = 1.1f;
+
+
+	private Vector2 startScale;
+	private Vector3 targetTween;
+	private Vector3 originalScale;
+
+	private void Start ()
+	{
+		startScale = transform.localScale;
+		originalScale = transform.localScale;
+		
+	}
+
+	private void FixedUpdate ()
+	{
+		var velocity = rigidbody.velocity.magnitude;
+
+		//if (Mathf.Approximately (velocity, 0f))
+		if (velocity < velocityGate)
+			transform.DOScale(originalScale, slowTweenTimer);
+			Debug.Log("Velocity majhen- grem na 1/1!");
+			//TODO: Ko se speeda up hočem animacijo potegnit iz 1,1 v karkoli bi moralo biti
+
+		var amount = velocity * strength + bias;
+		var inverseAmount = (1f / amount) * startScale.magnitude;
+
+
+		if (velocity > velocityGate)
+		{
+		
+			if (inverseAmount > yScaleLimit)
+				inverseAmount = yScaleLimit;
+			if (amount > xScaleLimit)
+				amount = xScaleLimit;
+			
+			switch (axis)
+			{
+				case Axis.X:
+					targetTween = new Vector3 (amount, inverseAmount, transform.rotation.eulerAngles.z);
+					transform.DOScale(targetTween,tweenTimer);
+					return;
+				case Axis.Y:
+					targetTween = new Vector3 (inverseAmount, amount, transform.rotation.eulerAngles.z);
+					transform.DOScale(targetTween,tweenTimer);
+					return;
+			}
+		}
+		
+	}
+}
---
+++ Assets/Scripts/tweenRotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Ludiq;
+using Bolt;
+
+public class tweenRotation : MonoBehaviour
+{
+    [SerializeField] Vector2 targetVector = new Vector3 (1, 2);
+    [SerializeField] float rotationTimer = 3f;
+    [SerializeField] Rigidbody2D rigidbody = default;
+    [SerializeField] float minVelocity = 0.1f;
+    public float angle;
+
+    private void FixedUpdate() {
+        
+        if (rigidbody.velocity.magnitude < minVelocity)
+            return;
+        
+        targetVector = (Vector2)Variables.Object(gameObject).Get("targetAngles");
+        angle = Vector2.SignedAngle (Vector2.up, targetVector);
+
+        if (angle > 15)
+            angle = 15;
+        if (angle < -50)
+            angle = -50;
+
+
+        var tarVector = new Vector3 (transform.position.x, transform.position.z, angle);
+        transform.DORotate(tarVector, rotationTimer).SetUpdate(UpdateType.Fixed, true);
+
+    }
+}
