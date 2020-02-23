1adcc65b650ec357ff70686b8ae33395d0aca9ad
Miha Frangež <miha.frangez@gmail.com>
2020-02-23 22:29:37 +0100

    Music Wrangler v2.0

--- Assets/Scripts/musicWrangler.cs
+++ Assets/Scripts/musicWrangler.cs
@@ -4,111 +4,108 @@ using UnityEngine;
 
 public class musicWrangler : MonoBehaviour
 {
-    public List<AudioClip> audioClips; // = new List<AudioClip>();
-    public List<AudioClip> transitionClips;
-    AudioSource musicSource;
-    AudioSource transitionSource;
-    AudioClip nextClip;
+    public List<AudioClip> audioClips;
 
-    //[Range(0,4)]
+    [Range(0,2)]
     public int clipNum = 0;
 
     int playingClipNum = 0;
-    bool isTransitioningOut = false;
+
+    AudioSource sourceA, sourceB;
+    AudioSource primarySource, secondarySource;
+
+    Coroutine primaryFadeRoutine = null;
+    Coroutine secondaryFadeRoutine = null;
 
 
-    // Start is called before the first frame update
     void Start()
     {
-        var sources = GetComponents<AudioSource>();
-        musicSource = sources[0];
-        transitionSource = sources[1];
+        AudioSource[] audioSources = gameObject.GetComponents<AudioSource>();
 
-        //musicSource.loop = true;
+        sourceA = audioSources[0];
+        sourceB = audioSources[1];
 
-        StartCoroutine(follower());
+        primarySource = sourceA;
+        secondarySource = sourceB;
+
+        CrossFade(audioClips[0], .5f, 3);
+    }
+    
+
+    void Update()
+    {
+        if (playingClipNum != clipNum)
+        {
+            CrossFade(audioClips[clipNum], .5f, 5);
+            playingClipNum = clipNum;
+        }
     }
 
-    IEnumerator follower()
+
+    public void CrossFade(AudioClip clipToPlay, float maxVolume, float fadingTime, float delay_before_crossFade = 0)
     {
-        Debug.Log("[MUSIC] Starting music coroutine");
-        musicSource.clip = audioClips[0];
+        StartCoroutine(CrossFadeCoroutine(clipToPlay, maxVolume, fadingTime, delay_before_crossFade));
+    }
+
+
+    IEnumerator CrossFadeCoroutine(AudioClip clipToPlay, float maxVolume, float fadingTime, float delay_before_crossFade = 0)
+    {
+        if (delay_before_crossFade > 0)
+        {
+            yield return new WaitForSeconds(delay_before_crossFade);
+        }
+
+        // Play secondary
+        secondarySource.clip = clipToPlay;
+        secondarySource.Play();
+        secondarySource.volume = 0;
+
+        // Prekini existing fade (just in case)
+        if (primaryFadeRoutine != null)
+            StopCoroutine(primaryFadeRoutine);
+        if (secondaryFadeRoutine != null)
+            StopCoroutine(secondaryFadeRoutine);
+
+        // Zaženi fade our primarne in fade in sekundarne
+        primaryFadeRoutine = StartCoroutine(FadeSourceCoroutine(primarySource, primarySource.volume, 0, fadingTime));
+        secondaryFadeRoutine = StartCoroutine(FadeSourceCoroutine(secondarySource, secondarySource.volume, maxVolume, fadingTime));
+
+        // Swap primary & secondary
+        var tmp = primarySource;
+        primarySource = secondarySource;
+        secondarySource = tmp;
+
+        yield break;
+    }
+
+
+    IEnumerator FadeSourceCoroutine(AudioSource sourceToFade, float startVolume, float endVolume, float duration)
+    {
+        float startTime = Time.time;
 
         while (true)
         {
-            Debug.Log("[MUSIC] (playing " + playingClipNum + " next " + clipNum + ")");
+            float elapsed = Time.time - startTime;
 
-            // Če je treba spremenit clip
-            if (clipNum != playingClipNum)
-            {
-                if (playingClipNum > transitionClips.Count)
-                    break;
+            sourceToFade.volume = Mathf.Clamp01(Mathf.Lerp(startVolume, endVolume, elapsed / duration));
 
-                Debug.Log("[MUSIC] Loading transition " + playingClipNum);
-                isTransitioningOut = true;
-                transitionSource.clip = transitionClips[playingClipNum];
+            if (sourceToFade.volume == endVolume)
+                break;
 
-                Debug.Log("[MUSIC] Waiting end of loop");
-                yield return new WaitWhile(() => musicSource.isPlaying);
-
-                Debug.Log("[MUSIC] Going to transition");
-                musicSource.Stop();
-                transitionSource.Play();
-
-                playingClipNum = clipNum;
-
-                yield return new WaitForSeconds(transitionSource.clip.length * .90f);
-            } else
-            // Ni treba spremenit clipa
-            {
-                // Je treba it iz transition -> loop
-                if (isTransitioningOut)
-                {
-                    if (clipNum > audioClips.Count)
-                        break;
-
-                    Debug.Log("[MUSIC] Loading loop " + clipNum);
-                    isTransitioningOut = false;
-                    musicSource.clip = audioClips[clipNum];
-
-                    Debug.Log("[MUSIC] Waiting end of transition");
-                    yield return new WaitWhile(() => transitionSource.isPlaying);
-
-                    Debug.Log("[MUSIC] Going to loop");
-                    musicSource.Play();
-                    transitionSource.Stop();
-                    
-
-                    yield return new WaitForSeconds(musicSource.clip.length * .90f);
-                } else
-                // Loop
-                {
-                    yield return new WaitWhile(() => musicSource.isPlaying);
-
-                    Debug.Log("[MUSIC] Ponavljam clip " + playingClipNum);
-                    musicSource.clip = audioClips[clipNum];
-                    musicSource.Play();
-
-                    Debug.Log("[MUSIC] Waiting end of loop");
-                    yield return new WaitForSeconds(musicSource.clip.length * .90f);
-                    //yield return new WaitWhile(() => musicSource.isPlaying);
-                }
-            }
-
-            
-
-            //Debug.Log("[MUSIC]   Čakam na konec predvajanja " + audio.clip.name);
-            //yield return new WaitForSeconds(audio.clip.length*.90f);
-            //Debug.Log("[MUSIC]       Predviden konec ");
-            //yield return new WaitWhile( () => audio.isPlaying );
-            //Debug.Log("[MUSIC]       Actual konec");
+            yield return null;
         }
-        Debug.Log("[MUSIC] Done");
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void Reset()
     {
-        
+        Update();
     }
+
+
+    void Awake()
+    {
+        Update();
+    }
+
 }
